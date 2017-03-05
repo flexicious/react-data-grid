9b0e0ce37c583bcbfc93fc007a4707315880205c
Flexicious <info@flexicious.com>
2017-03-04 21:02:21 -0500

    Added +import { +    CssStyles, ApiStyles +} from 'flexicious-react-datagrid-styles'

--- src/demo/components/LibraryImports.js
+++ src/demo/components/LibraryImports.js
@@ -3,8 +3,10 @@ import {
     UIUtils, ReactDataGrid, ReactDataGridColumnLevel, ReactDataGridColumn, ReactDataGridColumnGroup,
     Constants, ClassFactory, ComboBox, DatePicker, NumberFormatter, Filter, UIComponent, TypedObject, DateUtils, EventDispatcher, TextInput
 } from 'flexicious-react-datagrid'
-
+import {
+    CssStyles, ApiStyles
+} from 'flexicious-react-datagrid-styles'
 export {
-    UIUtils, ReactDataGrid, ReactDataGridColumnLevel, ReactDataGridColumn, ReactDataGridColumnGroup,
+    CssStyles, ApiStyles, UIUtils, ReactDataGrid, ReactDataGridColumnLevel, ReactDataGridColumn, ReactDataGridColumnGroup,
     Constants, ClassFactory, ComboBox, DatePicker, NumberFormatter, Filter, UIComponent, TypedObject, DateUtils, EventDispatcher, TextInput
 } 
