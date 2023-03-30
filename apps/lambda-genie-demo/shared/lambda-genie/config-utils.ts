208696ec3c573bde64a25c0cf1556aa99ba1d60f
flexicious <flexicious@gmail.com>
2023-03-29 20:25:31 -0400

    load from config

--- apps/lambda-genie-demo/shared/lambda-genie/config-utils.ts
+++ apps/lambda-genie-demo/shared/lambda-genie/config-utils.ts
@@ -5,7 +5,7 @@ import { getNodeJsConfigApi, ConfigApi } from "@euxdt/node-rules-engine";
 import { CONFIG } from "./config-bindings";
 import { mockConfig } from "./mock";
 
-const apiKey = "YOUR_KEY_HERE"; //replace with your api key
+const apiKey = process.env.LAMBDA_GENIE_API_KEY //"YOUR_KEY_HERE"; //replace with your api key
 const environment = CONFIG.ENVIRONMENTS.DEV;
 const headers =  {
     "x-api-key": apiKey,
@@ -17,26 +17,28 @@ export const loadConfigApi = async (lambdaName:string):Promise<ConfigApi> => {
             lambdaName,
             cacheDurationSeconds: 60,
             loadConfig: async (lastRefreshed?:Date, existingConfig?:ConfigJson) => {
-                // Once you have an API key, you can uncomment this code to load the config from the server
-                // if(lastRefreshed && existingConfig){
-                //     const configLastUpdated = await fetch("https://config.lambdagenie.com/lastUpdated", {
-                //         method: "POST",
-                //         headers
-                //     }).then((res) => res.json()).then((json) => new Date(json.updatedAt));
-                //     console.log("Last Refreshed", lastRefreshed, "Config Last Updated", configLastUpdated);
-                //     if(lastRefreshed && configLastUpdated && lastRefreshed.getTime() >= configLastUpdated.getTime()){
-                //         console.log("Config is up to date");
-                //         return existingConfig;  
-                //     }
-                // }
-                // console.log("Loading Config");
-                // const file = await fetch("https://config.lambdagenie.com/config", {
-                //     method: "POST",
-                //     headers
-                // }).then((res) => res.json());
-                // const configJson = file;
-                // console.log("Config Json", { configJson });
-                // return configJson as unknown as ConfigJson;
+                if(apiKey){
+                    console.log("Api Key Found", apiKey.length);
+                    if(lastRefreshed && existingConfig){
+                        const configLastUpdated = await fetch("https://config.lambdagenie.com/lastUpdated", {
+                            method: "POST",
+                            headers
+                        }).then((res) => res.json()).then((json) => new Date(json.updatedAt));
+                        console.log("Last Refreshed", lastRefreshed, "Config Last Updated", configLastUpdated);
+                        if(lastRefreshed && configLastUpdated && lastRefreshed.getTime() >= configLastUpdated.getTime()){
+                            console.log("Config is up to date");
+                            return existingConfig;  
+                        }
+                    }
+                    console.log("Loading Config");
+                    const file = await fetch("https://config.lambdagenie.com/config", {
+                        method: "POST",
+                        headers
+                    }).then((res) => res.json());
+                    const configJson = file;
+                    console.log("Config Json", { configJson });
+                    return configJson as unknown as ConfigJson;
+                }
 
                 return mockConfig as unknown as ConfigJson;
             },
