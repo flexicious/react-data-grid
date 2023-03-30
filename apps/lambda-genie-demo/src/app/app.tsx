d2e56cd099291539d3a0c45f4d5d83f41352d30a
flexicious <flexicious@gmail.com>
2023-03-29 20:16:34 -0400

    upgrade nx

--- apps/lambda-genie-demo/src/app/app.tsx
+++ apps/lambda-genie-demo/src/app/app.tsx
@@ -1,15 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <>
-      <NxWelcome title="nextjs-knex-react" />
-
       <div />
-    </>
   );
 }
 
