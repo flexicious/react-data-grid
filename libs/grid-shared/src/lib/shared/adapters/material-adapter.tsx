671b2bdd2887264f79d133fde8fb03b5a0147080
flexicious <flexicious@gmail.com>
2023-04-24 23:52:03 -0400

    fix mui adapter

--- libs/grid-shared/src/lib/shared/adapters/material-adapter.tsx
+++ libs/grid-shared/src/lib/shared/adapters/material-adapter.tsx
@@ -1,17 +1,22 @@
 import { CheckBoxState, ElementProps, GridCheckBoxProps, GridIconButton, GridMenuProps, LibraryAdapter, resolveExpression, TreeNodeType, VirtualTreeNode } from "@euxdt/grid-core";
 import {
     AddCircle, ArrowBack, ArrowForward, Cancel, ChevronRight, ContentPaste, CopyAll, DeleteOutline, DoneOutline, Edit, ExpandLess, ExpandMore, FileDownload, FilterAlt, FilterList, FirstPage, GridView, LastPage, PictureAsPdf, RemoveCircle, RestartAlt, Save, Settings,
-    UnfoldLess, UnfoldMore
+    UnfoldLess, UnfoldMore, Addchart, EditRoad
 } from "@mui/icons-material";
-import { Checkbox, Divider, IconButton, List, MenuItem, Select, TextField, Theme } from "@mui/material";
+import { Button, Checkbox, Divider, IconButton, List, MenuItem, Select, TextField, Theme } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { createElement, ReactNode, Ref } from "react";
-export const generateIcon = (icon: ReactNode, { onClick, disabled, title, className, style }: any) => 
-<IconButton color="primary" key={className} size="small" onClick={onClick} disabled={disabled} title={title} style={style}>
-    {icon}
-</IconButton>;
+import React from "react";
+
+export const generateIcon = (icon: ReactNode, { onClick, disabled, title, className, style }: any, textAndIcon?: boolean) =>
+    textAndIcon ? <Button color="primary" key={className} size="small" onClick={onClick} disabled={disabled} title={title} style={style} variant="outlined" startIcon={icon}>
+        {title}
+    </Button> :
+        <IconButton color="primary" key={className} size="small" onClick={onClick} disabled={disabled} title={title} style={style}>
+            {icon}
+        </IconButton>;
 
 
 export const materialNodePropsFunction = (theme: Theme) => (node: VirtualTreeNode, props: ElementProps) => {
@@ -30,7 +35,7 @@ export const materialNodePropsFunction = (theme: Theme) => (node: VirtualTreeNod
                 ...props,
                 style: {
                     ...props.style,
-                    backgroundColor: theme.palette.action.focus,
+                    backgroundColor: theme.palette.action.hover,
                     color: theme.palette.text.primary,
                 }
             };
@@ -39,7 +44,27 @@ export const materialNodePropsFunction = (theme: Theme) => (node: VirtualTreeNod
                 ...props,
                 style: {
                     ...props.style,
-                    backgroundColor: theme.palette.action.selected,
+                    backgroundColor: theme.palette.action.focus,
+                    color: theme.palette.text.primary,
+                }
+            };
+        }
+    } else if (node.type === TreeNodeType.Cell) {
+        if (props.className.indexOf("active-cell") >= 0) {
+            return {
+                ...props,
+                style: {
+                    ...props.style,
+                    backgroundColor: theme.palette.action.hover,
+                    color: theme.palette.text.primary,
+                }
+            };
+        } else if (props.className.indexOf("selected-cell") >= 0) {
+            return {
+                ...props,
+                style: {
+                    ...props.style,
+                    backgroundColor: theme.palette.action.focus,
                     color: theme.palette.text.primary,
                 }
             };
@@ -85,27 +110,29 @@ export const materialAdapter: LibraryAdapter = {
         </LocalizationProvider>;
     },
     createTextField: (props): unknown => {
-        const { onChange, placeholder, onKeyDown, ref, style, readOnly, onClick, value, defaultValue } = props;
+        const { onChange, placeholder, onKeyDown, ref, style, readOnly, onClick, value, defaultValue, attributes } = props;
         return <TextField value={value} defaultValue={defaultValue} onChange={(e) => {
             onChange?.(e.target.value);
-        }} onKeyDown={onKeyDown} inputProps={{ readOnly, onClick, placeholder }} style={style} inputRef={ref as Ref<any>} variant="standard" />;
+        }} onKeyDown={onKeyDown} inputProps={{ readOnly, onClick, placeholder }} style={style} inputRef={ref as Ref<any>} variant="standard" {...attributes} />;
     },
     createTextArea: (props): unknown => {
-        const { onChange, placeholder, onKeyDown, ref, style, readOnly, onClick, value, defaultValue } = props;
+        const { onChange, placeholder, onKeyDown, ref, style, readOnly, onClick, value, defaultValue, rows, attributes } = props;
         return <TextField value={value} defaultValue={defaultValue} onChange={(e) => {
             onChange?.(e.target.value);
-        }} onKeyDown={onKeyDown} inputProps={{ readOnly, onClick, placeholder }} style={style} inputRef={ref as Ref<any>} variant="standard" multiline rows={4} />;
+        }} onKeyDown={onKeyDown} inputProps={{ readOnly, onClick, placeholder }} style={style} inputRef={ref as Ref<any>} variant="standard" multiline rows={rows || 4}  {...attributes} />;
     },
     createCheckBox: (props: GridCheckBoxProps) => {
         const { onChange, value, ref } = props;
         return <Checkbox inputRef={ref as Ref<HTMLInputElement>} onChange={(e, checked) => { onChange?.(checked); }} checked={value === true} />;
     },
-    createMenu: (props: GridMenuProps)=> {
+    createMenu: (props: GridMenuProps) => {
         const { options } = props;
-        const cell = {rowIdentifier: props.rowPosition?.uniqueIdentifier ||"", 
-            columnIdentifier: props.columnPosition?.column?.uniqueIdentifier || ""};
+        const cell = {
+            rowIdentifier: props.rowPosition?.uniqueIdentifier || "",
+            columnIdentifier: props.columnPosition?.column?.uniqueIdentifier || ""
+        };
         return <List key="menu">
-            {options?.map((item,idx) =>item? <MenuItem key={idx} onClick={()=>item.onClick(cell)}>{item.label as ReactNode}</MenuItem>: <Divider key={idx} /> )}
+            {options?.map((item, idx) => item ? <MenuItem key={idx} onClick={() => item.onClick(cell)}>{item.label as ReactNode}</MenuItem> : <Divider key={idx} />)}
         </List>;
     },
     createTriStateCheckbox: (props): unknown => {
@@ -128,56 +155,60 @@ export const materialAdapter: LibraryAdapter = {
             indeterminate={value === CheckBoxState.INDETERMINATE}
             color="primary" />;
     },
-    createIconButton: (icon: GridIconButton, { props }): unknown => {
+    createIconButton: (icon: GridIconButton, { props }, textAndIcon?: boolean): unknown => {
 
         if (icon === GridIconButton.PageFirst) {
-            return generateIcon(<FirstPage />, props);
+            return generateIcon(<FirstPage />, props, textAndIcon);
         } else if (icon === GridIconButton.PagePrevious) {
-            return generateIcon(<ArrowBack />, props);
+            return generateIcon(<ArrowBack />, props, textAndIcon);
         } else if (icon === GridIconButton.PageNext) {
-            return generateIcon(<ArrowForward />, props);
+            return generateIcon(<ArrowForward />, props, textAndIcon);
         } else if (icon === GridIconButton.PageLast) {
-            return generateIcon(<LastPage />, props);
+            return generateIcon(<LastPage />, props, textAndIcon);
         } else if (icon === GridIconButton.Settings) {
-            return generateIcon(<Settings />, props);
+            return generateIcon(<Settings />, props, textAndIcon);
         } else if (icon === GridIconButton.SettingsSave) {
-            return generateIcon(<Save />, props);
+            return generateIcon(<Save />, props, textAndIcon);
         } else if (icon === GridIconButton.FilterBuilder) {
-            return generateIcon(<FilterList />, props);
+            return generateIcon(<FilterList />, props, textAndIcon);
+        } else if (icon === GridIconButton.ChartBuilder) {
+            return generateIcon(<Addchart />, props, textAndIcon);
         } else if (icon === GridIconButton.SettingsManage) {
-            return generateIcon(<GridView />, props);
+            return generateIcon(<GridView />, props, textAndIcon);
         } else if (icon === GridIconButton.ExpandOne) {
-            return generateIcon(<ExpandMore />, props);
+            return generateIcon(<ExpandMore />, props, textAndIcon);
         } else if (icon === GridIconButton.ExpandAll) {
-            return generateIcon(<UnfoldMore />, props);
+            return generateIcon(<UnfoldMore />, props, textAndIcon);
         } else if (icon === GridIconButton.CollapseOne) {
-            return generateIcon(<ExpandLess />, props);
+            return generateIcon(<ExpandLess />, props, textAndIcon);
         } else if (icon === GridIconButton.CollapseAll) {
-            return generateIcon(<UnfoldLess />, props);
+            return generateIcon(<UnfoldLess />, props, textAndIcon);
         } else if (icon === GridIconButton.Pdf) {
-            return generateIcon(<PictureAsPdf />, props);
+            return generateIcon(<PictureAsPdf />, props, textAndIcon);
         } else if (icon === GridIconButton.Excel) {
-            return generateIcon(<FileDownload />, props);
+            return generateIcon(<FileDownload />, props, textAndIcon);
         } else if (icon === GridIconButton.Cancel) {
-            return generateIcon(<Cancel />, props);
+            return generateIcon(<Cancel />, props, textAndIcon);
         } else if (icon === GridIconButton.Reset) {
-            return generateIcon(<RestartAlt />, props);
+            return generateIcon(<RestartAlt />, props, textAndIcon);
         } else if (icon === GridIconButton.Delete) {
-            return generateIcon(<DeleteOutline />, props);
+            return generateIcon(<DeleteOutline />, props, textAndIcon);
         } else if (icon === GridIconButton.Ok || icon === GridIconButton.Apply) {
-            return generateIcon(<DoneOutline />, props);
+            return generateIcon(<DoneOutline />, props, textAndIcon);
         } else if (icon === GridIconButton.Filter) {
-            return generateIcon(<FilterAlt />, props);
+            return generateIcon(<FilterAlt />, props, textAndIcon);
         } else if (icon === GridIconButton.Edit) {
-            return generateIcon(<Edit />, props);
+            return generateIcon(<Edit />, props, textAndIcon);
+        } else if (icon === GridIconButton.BulkEdit) {
+            return generateIcon(<EditRoad />, props, textAndIcon);
         } else if (icon === GridIconButton.Plus) {
-            return generateIcon(<AddCircle />, props);
+            return generateIcon(<AddCircle />, props, textAndIcon);
         } else if (icon === GridIconButton.Minus) {
-            return generateIcon(<RemoveCircle />, props);
+            return generateIcon(<RemoveCircle />, props, textAndIcon);
         } else if (icon === GridIconButton.Copy) {
-            return generateIcon(<CopyAll/>, props);
+            return generateIcon(<CopyAll />, props, textAndIcon);
         } else if (icon === GridIconButton.Paste) {
-            return generateIcon(<ContentPaste />, props);
+            return generateIcon(<ContentPaste />, props, textAndIcon);
         }
 
         return { props };
